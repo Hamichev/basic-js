100a333f3d9e3adbe434ec4847451821f87ac077
Hamichev <60568270+Hamichev@users.noreply.github.com>
2020-03-18 10:42:07 +0300

    init: create code

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,4 +1,14 @@
-module.exports = function createDreamTeam(/* members */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(members) {
+  let arr=[];
+  if(!Array.isArray(members)) {return false;}
+  for (let i=0;i<members.length;i++)
+  {
+    if (typeof(members[i])=="string")
+    {
+      members[i].toUpperCase().split('');
+      arr.push(members[i][0]);
+    }
+  }
+  if (arr.length != 0) {return arr.sort().join('');}
+  return false;
 };
